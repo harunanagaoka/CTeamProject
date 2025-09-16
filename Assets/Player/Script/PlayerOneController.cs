a6f418397f870c4d020bdf51c7c4b76eb2b2c3d8
MSI\litel <iayumi6052@gmail.com>
2025-09-16 16:45:13 +0900

    Playerの移動用スクリプト

---
+++ Assets/Player/Script/PlayerOneController.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerOneController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5f;
+    [SerializeField]
+    private float jumpForce = 5f;
+
+    private Rigidbody rigidBody;
+
+    bool isjumping_P1 = false;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float h1 = Input.GetAxis("Horizontal_P1");
+        float V1 = Input.GetAxis("Vertical_P1");
+        rigidBody.AddForce(Vector3.right * h1 * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        
+        if (!isjumping_P1 && Input.GetButtonDown("Jump_P1"))
+        {
+            {
+                rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                isjumping_P1 = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BeltConveyor"))
+        {
+            isjumping_P1 = false;
+        }
+    }
+}
---
+++ Assets/Player/Script/PlayerTwoController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class PlayerTwoController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+    [SerializeField]
+    float jumpForce = 5f;
+
+    private Rigidbody rigidBody;
+
+    bool isjumping_P2 = false;
+   
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h2 = Input.GetAxis("Horizontal_P2");
+        rigidBody.AddForce(Vector3.right * h2 * moveSpeed * Time.deltaTime,ForceMode.Impulse);
+        if (!isjumping_P2 && Input.GetButtonDown("Jump_P2"))
+        {
+           
+            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+             isjumping_P2 = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("BeltConveyor"))
+        {
+            isjumping_P2 = false;
+        }
+    }
+}
