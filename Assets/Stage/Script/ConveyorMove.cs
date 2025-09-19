3f2431de9ac019e25d2c7b5f0121c1d2edff47e6
harunanagaoka <shinkaigyo428@i.softbank.jp>
2025-09-19 15:25:29 +0900

    ステージ作成

--- Assets/Stage/Script/ConveyorMove.cs
+++ Assets/Stage/Script/ConveyorMove.cs
@@ -2,35 +2,12 @@ using UnityEngine;
 
 public class ConveyorMove : MonoBehaviour
 {
-    [SerializeField]
-    private float m_moveSpeed = 0;
+    [SerializeField] private Vector3 conveyorDirection = Vector3.right;
+    [SerializeField] private float conveyorSpeed = 3f;
 
-    [SerializeField]
-    private GameObject m_conveyor = null;
-
-    private Vector3 m_moveVector = Vector3.zero;
-
-    //private string m_tagName_one = "Player1";
-
-    //private string m_tagName_two = "Player2";
-
-    void Start()
+    // ベルト速度を取得する用
+    public Vector3 GetConveyorVelocity()
     {
-        m_moveVector = -m_conveyor.transform.forward * m_moveSpeed;
+        return conveyorDirection.normalized * conveyorSpeed;
     }
-
-
-    public void Move(Rigidbody rb)
-    {
-        rb.AddForce(m_moveVector, ForceMode.Acceleration);
-    }
-
-    //private void OnCollisionStay(Collision collision)
-    //{
-    //    if (collision.collider.tag == m_tagName_one || collision.collider.tag == m_tagName_two)
-    //    {
-    //        Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
-    //        rb.AddForce(m_moveVector, ForceMode.Acceleration);
-    //    }
-    //}
 }
